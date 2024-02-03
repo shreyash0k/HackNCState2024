f50873d03e5f01116d95eb088b4295ec19c53cba
shreyash <karandikarshreyash@gmail.com>
2024-02-03 15:55:14 -0500

    added material

---
+++ frontend/codevisor/src/app/layout/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styles: ``
+})
+export class FooterComponent {
+
+}
