d9fa16abd499c774d4675c4affc87c5b7f2b3ec4
aravinds13 <aravindtvm13@gmail.com>
2024-02-04 01:24:58 -0500

    WIP

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import axios from "axios";
+import crypto from "crypto";
 import { renderDOTToSVG } from "./renderer";
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
 const AUTH = process.env.OPEN_AI_AUTH;
 
-app.use(express.text());
+const mongoUrl = 'mongodb://localhost:27017';
+const mongodbName = 'projects_db';
+const mongoCollectionName = 'projects'
 
+app.use(express.text());
+app.use(express.json());
+
+//need to update //PUT //get ID, code
 app.post("/v1/post/chart", (request, response) => {
 	const query = "Please generate Graphviz code for a flowchart explaining the program below. Try to avoid including code in the flowchart. Instead, make it easily understandable with English explanations. Don't include any explanation in your response; rather, just generate the Graphviz code.\n"
 
@@ -49,6 +57,74 @@ app.post("/v1/post/chart", (request, response) => {
 				response.status(500).send('Failed to load SVG content');
 		}
 	})
+});
+
+//create new project
+app.post("/v1/post/project", (request, response) => {
+	const {project_name} = request.body;
+	const project_id = crypto.randomUUID();
+	const code = "";
+	const chart_svg = "";
+	const hash = "";
+	const timestamp = Date.now();
+
+	const createProject = async() => {
+		const client = new MongoClient(mongoUrl);
+		try{
+			await client.connect();
+			const db = client.db(mongodbName);
+			const collection = db.collection(mongoCollectionName);
+
+			const insertResult = await collection.insertOne({
+				project_id,
+				project_name,
+				code,
+				chart_svg,
+				hash,
+				timestamp
+			});
+			console.log(insertResult);
+			response.json({
+				project_id,
+				project_name,
+				code,
+				chart_svg,
+				timestamp
+			})
+		}
+		catch(error){
+			console.log(error)
+		}
+		finally{
+			await client.close();
+		}
+	}
+	createProject();
+});
+
+//get all projects from db
+app.get("/v1/get/projects", (_, response) => {
+
+	const fetchProjects = async() => {
+		const client = new MongoClient(mongoUrl);
+		try{
+			await client.connect();
+			const db = client.db(mongodbName);
+			const collection = db.collection(mongoCollectionName);
+
+			const fetchResult = await collection.find({}).toArray();
+			console.log(fetchResult);
+			response.json({"projects": [...fetchResult]});
+		}
+		catch(error){
+			console.log(error)
+		}
+		finally{
+			await client.close();
+		}
+	}
+	fetchProjects();
+
 })
 
 app.listen(port, () => {
