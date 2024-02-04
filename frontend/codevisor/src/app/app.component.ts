81e7bf571a4357b024a8a0d17a4d3043bfcafcda
jadenPete <jadenpete@github.com>
2024-02-03 21:43:31 -0500

    Made the "Generate" button print the editor contents

--- frontend/codevisor/src/app/app.component.ts
+++ frontend/codevisor/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from "@angular/core";
-
+import { Component, ViewChild } from "@angular/core";
 import { AngularSplitModule } from "angular-split";
 import { EditorComponent } from "./editor/editor.component"
 import { HeaderComponent } from "./layout/header/header.component"
@@ -8,8 +7,23 @@ import { PreviewComponent } from "./preview/preview.component";
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [  AngularSplitModule,EditorComponent, HeaderComponent, PreviewComponent],
-	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.css"
+	imports: [AngularSplitModule, EditorComponent, HeaderComponent, PreviewComponent],
+	templateUrl: "./app.component.html"
 })
-export class AppComponent {}
+export class AppComponent {
+	@ViewChild(EditorComponent) editor: EditorComponent | null = null;
+
+	generate() {
+		if (this.editor == null) {
+			console.error("Error: The editor hasn't been registered yet.");
+		} else {
+			const code = this.editor.getValue();
+
+			if (code == null) {
+				console.error("Error: Didn't get any code from the editor");
+			} else {
+				console.log(`Code: ${code}`);
+			}
+		}
+	}
+}
